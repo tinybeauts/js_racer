8687732e738353e1663f2b8616bcb3fe38d02936
Apprentice <apprentice@devbootcamp.com>
2013-04-16 23:43:26 -0700

    Js refactor for jQuery plugin

--- racer.js
+++ racer.js
@@ -30,22 +30,84 @@ MyGame.start_race = function() {
   $('.race_track li:nth-child(1)').addClass('active');
 }
 
+jQuery.prototype.jsRacer = function(opts) {
+  opts = opts || {};
+  var trackLength = opts.trackLength || 10;
+  var players     = 2;
+  var keys        = [16, 13];
+
+  
+
+  // MODEL!
+  var keyMap          = {16: 0, 13: 1};
+  var positions       = {0: 0, 1: 0};
+  var winner          = null;
+
+  
+  // VIEW!
+  var view = this;
+  view.paint = function(){
+    for(var i = 0; i < players; i++){
+      this.append($('<ul>'));
+      for(var j = 0; j < trackLength; j++){
+        this.children().last().append($('<li>'));
+      }
+    }
+  }
+
+  view.updatePosition = function(){
+    this.find('li').removeClass('active');
+    for(var i = 0; i < players; i ++){
+      console.log(i, positions[i])
+      this.children().eq(i).children().eq([positions[i]]).addClass('active');
+    }
+  }
+
+  //CONTROLLER!
+  view.paint();
+  view.updatePosition();
+  
+  $(document).on('keyup', function(event) {
+    var key = event.which;
+    var player = keyMap[key];
+    positions[player] += 1;
+
+    if (positions[player] >= trackLength) {
+      winner = player;
+      // stop the game somehow and eat juice
+    }
+
+    view.updatePosition();
+
+  });
+
+  this.reset = function(){
+    positions = 0;
+    winner = null;
+  }
+
+
+  return this;
+}
+
 
 
 $(document).ready(function() {
-  MyGame.start_race();
-
-  $(document).on('keyup', function(event) {
-    var key = event.which
-    if (key == 16) {  
-      MyGame.update_player_position(1);
-    } else if (key == 13) {
-      MyGame.update_player_position(2);
-    };
+  var foo = $('.race-track').jsRacer({
+    trackLength: 20
   });
 
-  $('#restart').on('click', function() {
-    MyGame.start_race();
-    $('.race_track h2').html(" ");
-  });
+  // $(document).on('keyup', function(event) {
+  //   var key = event.which
+  //   if (key == 16) {  
+  //     MyGame.update_player_position(1);
+  //   } else if (key == 13) {
+  //     MyGame.update_player_position(2);
+  //   };
+  // });
+
+  // $('#restart').on('click', function() {
+  //   MyGame.start_race();
+  //   $('.race_track h2').html(" ");
+  // });
 });
